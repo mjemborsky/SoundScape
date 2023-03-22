98b1019c97c3091ca6a3fbe1e4d081882c64b50c
mjemborsky <michaelemborsky@gmail.com>
2023-03-22 16:21:30 -0400

    Update ContentView.swift

--- code-ios/SoundScape/SoundScape/ContentView.swift
+++ code-ios/SoundScape/SoundScape/ContentView.swift
@@ -17,10 +17,32 @@ struct ContentView: View {
     
     var body: some View {
         VStack() {
-            Text("\(hostModel.viewModel.title )")
-                .textSelection(.enabled)
-                .padding()
+            HStack(spacing: 59) {
+                Spacer()
+                Text("SoundScape")
+                    .textSelection(.enabled)
+                Spacer()
+                HStack() {
+                    Image(systemName: "person.crop.circle.fill")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 25, height: 25)
+                                        .clipShape(Circle())
+                    Text("Profile/Settings")
+                }
+            }
+            .padding()
             VStack(alignment: .center) {
+                if let viewController = hostModel.viewModel.viewController {
+                    AUViewControllerUI(viewController: viewController)
+                        .padding(margin)
+                } else {
+                    VStack() {
+                        Text(hostModel.viewModel.message)
+                            .padding()
+                    }
+                    .frame(minWidth: 400, minHeight: 200)
+                }
                 if let viewController = hostModel.viewModel.viewController {
                     AUViewControllerUI(viewController: viewController)
                         .padding(margin)
@@ -34,18 +56,64 @@ struct ContentView: View {
             }
             .padding(doubleMargin)
             
-            if hostModel.viewModel.showAudioControls {
-                Text("Audio Playback")
-                Button {
-                    hostModel.isPlaying ? hostModel.stopPlaying() : hostModel.startPlaying()
-                    
-                } label: {
-                    Text(hostModel.isPlaying ? "Stop" : "Play")
+            HStack() {
+                if hostModel.viewModel.showAudioControls {
+                    Button {
+                        hostModel.isPlaying ? hostModel.stopPlaying() : hostModel.startPlaying()
+                        
+                    } label: {
+                        Text(hostModel.isPlaying ? "Preset 1" : "Preset 1")
+                    }
+                }
+                if hostModel.viewModel.showMIDIContols {
+                    Text("MIDI Input: Enabled")
+                }
+                if hostModel.viewModel.showAudioControls {
+                    Button {
+                        hostModel.isPlaying ? hostModel.stopPlaying() : hostModel.startPlaying()
+                        
+                    } label: {
+                        Text(hostModel.isPlaying ? "Preset 2" : "Preset 2")
+                    }
+                }
+                if hostModel.viewModel.showMIDIContols {
+                    Text("MIDI Input: Enabled")
+                }
+                if hostModel.viewModel.showAudioControls {
+                    Button {
+                        hostModel.isPlaying ? hostModel.stopPlaying() : hostModel.startPlaying()
+                        
+                    } label: {
+                        Text(hostModel.isPlaying ? "Preset 3" : "Preset 3")
+                    }
+                }
+                if hostModel.viewModel.showMIDIContols {
+                    Text("MIDI Input: Enabled")
+                }
+                if hostModel.viewModel.showAudioControls {
+                    Button {
+                        hostModel.isPlaying ? hostModel.stopPlaying() : hostModel.startPlaying()
+                        
+                    } label: {
+                        Text(hostModel.isPlaying ? "Preset 4" : "Preset 4")
+                    }
+                }
+                if hostModel.viewModel.showMIDIContols {
+                    Text("MIDI Input: Enabled")
+                }
+                if hostModel.viewModel.showAudioControls {
+                    Button {
+                        hostModel.isPlaying ? hostModel.stopPlaying() : hostModel.startPlaying()
+                        
+                    } label: {
+                        Text(hostModel.isPlaying ? "Preset 5" : "Preset 5")
+                    }
+                }
+                if hostModel.viewModel.showMIDIContols {
+                    Text("MIDI Input: Enabled")
                 }
             }
-            if hostModel.viewModel.showMIDIContols {
-                Text("MIDI Input: Enabled")
-            }
+            
             Spacer()
                 .frame(height: margin)
         }
