be1e35d780892ee770320c73f95d0a2aeacce4c9
mjemborsky <michaelemborsky@gmail.com>
2023-10-23 13:29:23 -0400

    reset

--- mac/SoundScape/SoundScapeExtension/Common/SoundScapeExtension-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  SoundScapeExtension-Bridging-Header.h
-//  SoundScapeExtension
-//
-//  Created by Michael Emborsky on 2/16/23.
-//
-
-#import "SoundScapeExtensionAudioUnit.h"
-#import "SoundScapeExtensionParameterAddresses.h"
