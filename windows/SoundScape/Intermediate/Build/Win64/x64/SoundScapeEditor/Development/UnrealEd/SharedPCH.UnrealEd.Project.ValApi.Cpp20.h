2bbe0e298277f231a1a09c7efedb8fd05112deb3
mjemborsky <33811013+mjemborsky@users.noreply.github.com>
2024-08-10 00:01:06 -0500

    Unreal

---
+++ windows/SoundScape/Intermediate/Build/Win64/x64/SoundScapeEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/Users/micha/OneDrive/Desktop/Personal/Soundscape/SoundScape/windows/SoundScape/Intermediate/Build/Win64/x64/SoundScapeEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
